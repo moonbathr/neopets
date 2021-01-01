a2d84e8aa81a52f4351943aa13b2aaa5f2497ad9
moonbathr <76828340+moonbathr@users.noreply.github.com>
2021-01-01 15:02:52 -0500

    Made content background transparent.
    * moved this aesthetic change specific to the Neoboards over from the theme CSS because it adversely impacted other parts of the beta site (gray BG everywhere, items with white BGs displayed over top).

--- bookmark_boards.js
+++ bookmark_boards.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Neopets: Bookmark Boards
-// @version      1.0
+// @version      1.1
 // @description  Adds selected boards to the top
 // @author       sunbathr
 // @match        http://www.neopets.com/neoboards/*
@@ -12,6 +12,7 @@ $(`<style type='text/css'>
 #bookmarked_boards td { border: 1px solid #efefef; padding: 10px; font-family: "MuseoSansRounded500", 'Arial', sans-serif;  valign : middle; font-size: 11px; width: 150px; }
 #bookmarked_boards td a:link, #bookmarked_boards td a:visited { text-decoration: none; color: #3b54b4; font-weight: bold; font-size: 15px; line-height: 25px; }
 #bookmarked_boards td img { float: left; margin-right: 10px;}
+div.container.theme-bg { background-color: transparent; }
 }
 </style>`).appendTo("head");
 
