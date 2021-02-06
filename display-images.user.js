9509f1b3b5cec521f6ed7f6e448fc026a15bd3b4
moonbathr <76828340+moonbathr@users.noreply.github.com>
2021-02-06 11:53:06 -0800

    Update display-images.user.js

--- display-images.user.js
+++ display-images.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Neopets: Add Images to Neoboards
+// @name         Neopets: Add Images
 // @description  Changes image links to images
 // @author       sunbathr + rawbeee
 // @match        http://www.neopets.com/neoboards/*
@@ -17,4 +17,18 @@ function addImages() {
     });
 }
 
-addImages();
+function addSearch() {
+    var html = `<div id="slothsearch" style="margin-top: 30px; margin-left:-10px;">
+<form method="get" action="http://www.drsloth.com/process/" data-abide="" novalidate="novalidate" target="_blank">
+          <div class="row">
+            <div class="small-8 columns">
+              <input id="search-tagged" name="tagged" type="text" value="" style="border: 1px solid #cacaca; border-radius: 5px;" title="Try including 'animated' to find gifs">
+            </div>
+          <input type="submit" value="Search DrSloth" class="slothsearchbutton topicReplySubmit topicCreateSubmit" style="margin: 5px; width: 110px; font-size: 14px; height: 25px;">
+        </form>
+        </div>`;
+    $(`.replySmilies-neoboards`).append(html)
+    $(`.topicCreateSmilies-neoboards`).append(html)
+}
+document.addEventListener('DOMContentLoaded', addSearch);
+document.addEventListener('DOMContentLoaded', addImages);
